3098684e6337df2c769bea5532cb7cb2a686568e
Kong <congcoi123@gmail.com>
2022-06-06 22:10:09 +0200

    feat: Infinite List and Saved Favorite List

--- flutter_example/lib/main.dart
+++ flutter_example/lib/main.dart
@@ -1,7 +1,6 @@
 // Copyright 2018 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import 'dart:math';
 
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
@@ -17,16 +16,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Infinite Scrolling List',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Infinite Scrolling List'),
+        debugShowCheckedModeBanner: false,
+        title: 'Infinite Scrolling List and Favorite',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black
+          )
         ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
-    );
+        home: const RandomWords());
   }
 }
 
@@ -39,31 +37,85 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 10);
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18);
   final _logger = Logger();
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final titles = _saved.map(
+              (pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              }
+          );
+          final divided = titles.isNotEmpty ? ListTile.divideTiles(context: context, tiles: titles).toList() : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        }
+      )
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) {
-          return const Divider();
-        }
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Infinite Scrolling List and Favorite'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: _pushSaved,
+              tooltip: "Saved Suggestions",
+            ),
+          ],
+        ),
+        body: ListView.builder(
+            padding: const EdgeInsets.all(16.0),
+            itemBuilder: (context, i) {
+              if (i.isOdd) {
+                return const Divider();
+              }
 
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _logger.i("List: $_suggestions");
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont
-          )
-        );
-      }
-    );
+              final index = i ~/ 2;
+              if (index >= _suggestions.length) {
+                // _logger.i("List: $_suggestions");
+                _suggestions.addAll(generateWordPairs().take(10));
+              }
+
+              final alreadySaved = _saved.contains(_suggestions[index]);
+
+              return ListTile(
+                title:
+                    Text(_suggestions[index].asPascalCase, style: _biggerFont),
+                trailing: Icon(
+                  alreadySaved ? Icons.favorite : Icons.favorite_border,
+                  color: alreadySaved ? Colors.red : null,
+                  semanticLabel: alreadySaved ? "Remove from saved" : "Save",
+                ),
+                onTap: () {
+                  setState(() {
+                    // var indexValue = _suggestions[index];
+                    // _logger.i("TAP: $indexValue");
+                    if (alreadySaved) {
+                      _saved.remove(_suggestions[index]);
+                    } else {
+                      _saved.add(_suggestions[index]);
+                    }
+                  });
+                },
+              );
+            }));
   }
 }
-
